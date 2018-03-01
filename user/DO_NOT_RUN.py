92411e3a42111642e814c8d4f9d2a199e84bb5a2
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-03-01 12:34:22 +1100

    CROWE, Vincent: 2018-03-01 12:34:22

---
+++ user/DO_NOT_RUN.py
@@ -0,0 +1,7 @@
+x = 0
+import os
+while x == 0:
+    print ('SHIT')
+    os.system("start C:/Users/Vincent/Desktop/FilthyRoleplay/OHNO.py")
+    os.system("start C:/Users/Vincent/Desktop/FilthyRoleplay/OHNO.py")
+    os.system("start C:/Users/Vincent/Desktop/FilthyRoleplay/OHNO.py")
---
+++ user/R1.py
@@ -0,0 +1,14 @@
+import os
+import time
+x = input ('')
+if x == 'run':
+    print ('WARNING WARNING')
+    y = 10
+    print (10)
+    while y > 0:
+        y=y-1
+        print (y), time.sleep(1)
+
+if y == (0):
+    os.remove("C:/Users/Vincent/Desktop/FilthyRoleplay/R1-Copy.py")
+    
